34218caa53c010d7f90f31bb7ba61faa761f8030
iliasseAbdessamad <samwillh7@gmail.com>
2025-08-13 08:20:57 +0100

    First project setup : (Add dependencies - Add application.properties in .gitignore file - create application.properties.example file)

---
+++ src/main/java/com/genai/GenAiApplication.java
@@ -0,0 +1,14 @@
+package com.genai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GenAiApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(GenAiApplication.class, args);
+	}
+
+}
